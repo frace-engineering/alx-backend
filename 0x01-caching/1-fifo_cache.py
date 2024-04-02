af9089ed461ebdd8356667557dd8ea8df607134e
Engr Ugwu <fugwungene@gmail.com>
2024-04-02 23:08:12 +0000

    My first commit

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    FIFOCache class that inherits from BaseCaching
+    """
+
+    def __init__(self):
+        """
+        Initialize FIFOCache
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Add an item to the cache
+        """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                # Find the first key inserted into the cache
+                first_key = next(iter(self.cache_data))
+                print("DISCARD: {}".format(first_key))
+                del self.cache_data[first_key]
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieve an item from the cache
+        """
+        if key is not None:
+            return self.cache_data.get(key)
+        return None
---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    LIFOCache class that inherits from BaseCaching
+    """
+
+    def __init__(self):
+        """
+        Initialize LIFOCache
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Add an item to the cache
+        """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                # Find the last key inserted into the cache
+                last_key = list(self.cache_data.keys())[-1]
+                print("DISCARD: {}".format(last_key))
+                del self.cache_data[last_key]
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieve an item from the cache
+        """
+        if key is not None:
+            return self.cache_data.get(key)
+        return None
